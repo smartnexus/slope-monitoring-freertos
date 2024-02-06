b7d09240f64b8c968a2a8f6de0189f7b2c3f0859
smartnexus <angrodboh@alum.us.es>
2024-02-06 10:54:07 +0100

    Version final

--- a.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def calcular_angulo_z(aceleracion_x, aceleracion_y, aceleracion_z):
-    angulo_z = math.atan2(math.sqrt(aceleracion_x**2 + aceleracion_y**2), aceleracion_z)
-    return math.degrees(angulo_z)
